ab7539dde7279c238187e7d63139f75f50aa164b
JanithNimhara <137036759+janithnm@users.noreply.github.com>
2026-05-07 16:12:34 +0530

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/silverlineit/coursecontentsystem/CoursecontentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.silverlineit.coursecontentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursecontentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursecontentsystemApplication.class, args);
+	}
+
+}
